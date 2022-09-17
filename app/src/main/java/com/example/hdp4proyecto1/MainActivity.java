752f5014ed3be1092a56b6e592795b7e8b89c6eb
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2022-09-17 16:41:04 -0500

    Updated id formatting

--- app/src/main/java/com/example/hdp4proyecto1/MainActivity.java
+++ app/src/main/java/com/example/hdp4proyecto1/MainActivity.java
@@ -1,22 +1,21 @@
 package com.example.hdp4proyecto1;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.common.base.Strings;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,34 +31,43 @@ public class MainActivity extends AppCompatActivity {
         cedula = findViewById(R.id.et_cedula);
         votar = findViewById(R.id.btn_votar);
         votar.setOnClickListener(view -> {
-            String[] parts = cedula.getText().toString().split("-");
-            String part1 = String.format(Locale.getDefault(), "%02d", Integer.parseInt(parts[0]));
-            String part2 = String.format(Locale.getDefault(),"%04d", Integer.parseInt(parts[1]));
-            String part3 = String.format(Locale.getDefault(),"%06d", Integer.parseInt(parts[2]));
-            String cedulaZeros = part1 + "-" + part2 + "-" + part3;
-            Query queryCedula = myRef.orderByChild("cedula").equalTo(cedulaZeros);
-            Log.d("Cedula", cedulaZeros);
-            queryCedula.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    if(dataSnapshot.exists()) {
-                        for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
-                            String uid = childSnapshot.getKey();
-                            Log.d("Firebase database", "Key is: " + uid);
+            if (cedula.getText().toString().equals("")) {
+                Toast.makeText(getApplicationContext(), "Ingrese su cedula",
+                        Toast.LENGTH_LONG).show();
+            }
+            else if (!cedula.getText().toString().contains("-")) {
+                Toast.makeText(getApplicationContext(), "Debe ingresar su cedula con guion",
+                        Toast.LENGTH_LONG).show();
+            }
+            else {
+                String[] parts = cedula.getText().toString().split("-");
+                String part1 = Strings.padStart(parts[0], 2, '0');
+                String part2 = Strings.padStart(parts[1], 4, '0');
+                String part3 = Strings.padStart(parts[2], 6, '0');
+                String cedulaZeros = part1 + "-" + part2 + "-" + part3;
+                Query queryCedula = myRef.orderByChild("cedula").equalTo(cedulaZeros);
+                Log.d("Cedula", cedulaZeros);
+                queryCedula.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
+                                String uid = childSnapshot.getKey();
+                                Log.d("Firebase database", "Key is: " + uid);
+                            }
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Cedula no encontrada", Toast.LENGTH_LONG).show();
+                            Log.d("Firebase database", "No existe");
                         }
                     }
-                    else {
-                        Toast.makeText(getApplicationContext(), "Cedula no encontrada", Toast.LENGTH_SHORT).show();
-                        Log.d("Firebase database", "No existe");
-                    }
-                }
 
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-                    // Failed to read value
-                    Log.w("Firebase database", "Failed to read value.", error.toException());
-                }
-            });
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        // Failed to read value
+                        Log.w("Firebase database", "Failed to read value.", error.toException());
+                    }
+                });
+            }
         });
 
     }
