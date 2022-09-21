8c3f58865ecacc1d4da38bf3ef03477c528055af
Bryan <49354825+bryanjtc@users.noreply.github.com>
2022-09-21 14:47:07 -0500

    Added code to calculate and display results

--- app/src/main/java/com/example/hdp4proyecto1/Votos.java
+++ app/src/main/java/com/example/hdp4proyecto1/Votos.java
@@ -1,10 +1,12 @@
 package com.example.hdp4proyecto1;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -13,16 +15,23 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Locale;
+
 public class Votos extends AppCompatActivity {
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("data");
     Long votosVivian, votosMartin, votosOmar;
+    SeekBar sbVivian, sbMartin, sbOmar;
+    TextView tvVivian, tvMartin, tvOmar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_votos);
+        double totalVotos, porcentajeVivian, porcentajeMartin, porcentajeOmar;
+        // Todo inicializar seekbar y button con los id del layout
+
         Query queryVivian = myRef.orderByChild("Cedula").equalTo("Vivían Valenzuela");
         Query queryMartin = myRef.orderByChild("Cedula").equalTo("Martín Candanedo");
         Query queryOmar = myRef.orderByChild("Cedula").equalTo("Omar Aizpurua");
@@ -62,5 +71,15 @@ public class Votos extends AppCompatActivity {
                 Log.w("Firebase database", "Failed to read value.", error.toException());
             }
         });
+        totalVotos = votosVivian + votosMartin + votosOmar;
+        porcentajeVivian = (votosVivian/totalVotos)*100;
+        porcentajeMartin = (votosMartin/totalVotos)*100;
+        porcentajeOmar = (votosOmar/totalVotos)*100;
+        sbVivian.setProgress((int) porcentajeVivian);
+        sbMartin.setProgress((int) porcentajeMartin);
+        sbOmar.setProgress((int) porcentajeOmar);
+        tvVivian.setText(String.format(Locale.getDefault(), "%.0f%%",porcentajeVivian));
+        tvMartin.setText(String.format(Locale.getDefault(),"%.0f%%",porcentajeMartin));
+        tvOmar.setText(String.format(Locale.getDefault(),"%.0f%%",porcentajeOmar));
     }
 }
