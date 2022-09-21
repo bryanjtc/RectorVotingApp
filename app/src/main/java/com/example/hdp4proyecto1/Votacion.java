390d0d467b095fa3a20032f0d5f7252736f4deee
Bryan <49354825+bryanjtc@users.noreply.github.com>
2022-09-21 14:21:58 -0500

    Added events to radiobutton and button for voting

--- app/src/main/java/com/example/hdp4proyecto1/Votacion.java
+++ app/src/main/java/com/example/hdp4proyecto1/Votacion.java
@@ -1,24 +1,47 @@
 package com.example.hdp4proyecto1;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class Votacion extends AppCompatActivity {
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("data");
+    RadioButton rbCandidato1, rbCandidato2, rbCandidato3;
+    Button btnVotar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_votacion);
+        String[] candidatos = new String[]{"Vivían Valenzuela", "Omar Aizpurua", "Martín Candanedo"};
+        AtomicReference<Integer> seleccion = new AtomicReference<>(-1);
+        // Todo inicializar radiobuttons y button con los id del layout
+
         // Todo utilizar variable key del intent
         myRef.child("key").child("Votado").setValue(true);
-        // Todo utilizar variable del nombre del candidato
-        myRef.child("key").child("Voto").setValue("candidato");
+        rbCandidato1.setOnClickListener(view -> seleccion.set(0));
+        rbCandidato2.setOnClickListener(view -> seleccion.set(1));
+        rbCandidato3.setOnClickListener(view -> seleccion.set(2));
+        btnVotar.setOnClickListener(view -> {
+            if (seleccion.get() != -1) {
+                // Todo utilizar variable key del intent
+                myRef.child("key").child("Voto").setValue(candidatos[seleccion.get()]);
+                Log.d("Votacion", "Voto es: " + candidatos[seleccion.get()]);
+            } else {
+                Toast.makeText(getApplicationContext(), "Debe seleccionar un candidato",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
